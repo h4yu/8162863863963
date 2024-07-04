2bd6a4663c6f1e87d7e97492ba27679f7509f9c2
Wuynnz <158767658+Wuynnz@users.noreply.github.com>
2024-07-04 10:56:36 +0700

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -1,28 +1,3 @@
-for _, v in ipairs(game.CoreGui:GetChildren()) do
-    if v.Name == "ScreenGui" or v.Name == "" then
-        v:Destroy()
-    end
-end
-local ScreenGui = Instance.new("ScreenGui")
-local ImageButton = Instance.new("ImageButton")
-local UICorner = Instance.new("UICorner")
-        
-ScreenGui.Name = ""
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-UILock = true
-ImageButton.Parent = ScreenGui
-ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-ImageButton.BorderSizePixel = 0
-ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
-ImageButton.Size = UDim2.new(0, 50, 0, 50)
-ImageButton.Draggable = true
-ImageButton.Image = "http://www.roblox.com/asset/?id=18318099219"
-ImageButton.MouseButton1Down:Connect(function()
-	game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" , false , game)
-end)
-UICorner.Parent = ImageButton
-
 
 local a, b = {
     {
