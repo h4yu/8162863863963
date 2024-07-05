287dee062d9934ecaca9fceaf9b6ca781ce97912
Wuynnz <158767658+Wuynnz@users.noreply.github.com>
2024-07-05 10:23:12 +0700

    Update InterfaceManager.lua

--- Addons/InterfaceManager.lua
+++ Addons/InterfaceManager.lua
@@ -1,60 +1,51 @@
-local HttpService = game:GetService("HttpService")
+local httpService = game:GetService("HttpService")
 
-local InterfaceManager = {}
-do
-    InterfaceManager.Folder = "FluentSettings"
+local InterfaceManager = {} do
+	InterfaceManager.Folder = "FluentSettings"
     InterfaceManager.Settings = {
-        Theme = "Amethyst",
-        Acrylic = false,
+        Theme = "Dark",
+        Acrylic = true,
         Transparency = true,
         MenuKeybind = "F6"
     }
 
     function InterfaceManager:SetFolder(folder)
-        self.Folder = folder
-        self:BuildFolderTree()
-    end
+		self.Folder = folder;
+		self:BuildFolderTree()
+	end
 
     function InterfaceManager:SetLibrary(library)
-        self.Library = library
-    end
+		self.Library = library
+	end
 
     function InterfaceManager:BuildFolderTree()
-        local paths = {}
+		local paths = {}
 
-        local parts = self.Folder:split("/")
-        for idx = 1, #parts do
-            paths[#paths + 1] = table.concat(parts, "/", 1, idx)
-        end
+		local parts = self.Folder:split("/")
+		for idx = 1, #parts do
+			paths[#paths + 1] = table.concat(parts, "/", 1, idx)
+		end
 
-        table.insert(paths, self.Folder)
-        table.insert(paths, self.Folder .. "/settings")
+		table.insert(paths, self.Folder)
+		table.insert(paths, self.Folder .. "/settings")
 
-        for i = 1, #paths do
-            local str = paths[i]
-            if not isfolder(str) then
-                makefolder(str)
-            end
-        end
-    end
+		for i = 1, #paths do
+			local str = paths[i]
+			if not isfolder(str) then
+				makefolder(str)
+			end
+		end
+	end
 
     function InterfaceManager:SaveSettings()
-        local Success, ErrorMessage =
-            pcall(
-            function()
-                writefile(self.Folder .. "/options.json", HttpService:JSONEncode(InterfaceManager.Settings))
-            end
-        )
-        if not Success then
-            warn("Interface Manager:", ErrorMessage)
-        end
+        writefile(self.Folder .. "/options.json", httpService:JSONEncode(InterfaceManager.Settings))
     end
 
     function InterfaceManager:LoadSettings()
         local path = self.Folder .. "/options.json"
         if isfile(path) then
             local data = readfile(path)
-            local success, decoded = pcall(HttpService.JSONDecode, HttpService, data)
+            local success, decoded = pcall(httpService.JSONDecode, httpService, data)
 
             if success then
                 for i, v in next, decoded do
@@ -66,69 +57,57 @@ do
 
     function InterfaceManager:BuildInterfaceSection(tab)
         assert(self.Library, "Must set InterfaceManager.Library")
-        local Library = self.Library
+		local Library = self.Library
         local Settings = InterfaceManager.Settings
 
         InterfaceManager:LoadSettings()
 
-        local section = tab:AddSection("Interface")
+		local section = tab:AddSection("Interface")
 
-        local InterfaceTheme =
-            section:AddDropdown(
-            "InterfaceTheme",
-            {
-                Title = "Theme",
-                Description = "Changes the interface theme.",
-                Values = Library.Themes,
-                Default = Settings.Theme,
-                Callback = function(Value)
-                    Library:SetTheme(Value)
-                    Settings.Theme = Value
-                    InterfaceManager:SaveSettings()
-                end
-            }
-        )
+		local InterfaceTheme = section:AddDropdown("InterfaceTheme", {
+			Title = "Theme",
+			Description = "Changes the interface theme.",
+			Values = Library.Themes,
+			Default = Settings.Theme,
+			Callback = function(Value)
+				Library:SetTheme(Value)
+                Settings.Theme = Value
+                InterfaceManager:SaveSettings()
+			end
+		})
 
         InterfaceTheme:SetValue(Settings.Theme)
-
-        if Library.UseAcrylic then
-            section:AddToggle(
-                "AcrylicToggle",
-                {
-                    Title = "Acrylic",
-                    Description = "The blurred background requires graphic quality 8+",
-                    Default = Settings.Acrylic,
-                    Callback = function(Value)
-                        Library:ToggleAcrylic(Value)
-                        Settings.Acrylic = Value
-                        InterfaceManager:SaveSettings()
-                    end
-                }
-            )
-        end
-
-        section:AddToggle(
-            "TransparentToggle",
-            {
-                Title = "Transparency",
-                Description = "Makes the interface transparent.",
-                Default = Settings.Transparency,
-                Callback = function(Value)
-                    Library:ToggleTransparency(Value)
-                    Settings.Transparency = Value
+	
+		if Library.UseAcrylic then
+			section:AddToggle("AcrylicToggle", {
+				Title = "Acrylic",
+				Description = "The blurred background requires graphic quality 8+",
+				Default = Settings.Acrylic,
+				Callback = function(Value)
+					Library:ToggleAcrylic(Value)
+                    Settings.Acrylic = Value
                     InterfaceManager:SaveSettings()
-                end
-            }
-        )
-
-        local MenuKeybind = section:AddKeybind("MenuKeybind", {Title = "Minimize Bind", Default = Settings.MenuKeybind})
-        MenuKeybind:OnChanged(
-            function()
-                Settings.MenuKeybind = MenuKeybind.Value
+				end
+			})
+		end
+	
+		section:AddToggle("TransparentToggle", {
+			Title = "Transparency",
+			Description = "Makes the interface transparent.",
+			Default = Settings.Transparency,
+			Callback = function(Value)
+				Library:ToggleTransparency(Value)
+				Settings.Transparency = Value
                 InterfaceManager:SaveSettings()
-            end
-        )
-        Library.MinimizeKeybind = Enum.KeyCode.F6
+			end
+		})
+	
+		local MenuKeybind = section:AddKeybind("MenuKeybind", { Title = "Minimize Bind", Default = Settings.MenuKeybind })
+		MenuKeybind:OnChanged(function()
+			Settings.MenuKeybind = MenuKeybind.Value
+            InterfaceManager:SaveSettings()
+		end)
+		Library.MinimizeKeybind = MenuKeybind
     end
 end
 
