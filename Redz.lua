1625f4bc64b5cc1935d94927319cc65da6d53dca
Wuynnz <158767658+Wuynnz@users.noreply.github.com>
2024-07-06 22:54:31 +0700

    Update Redz.lua

--- Redz.lua
+++ Redz.lua
@@ -47,6 +47,7 @@ local redzlib = {
       ["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
     }
   },
+  
   Info = {
     Version = "1.0.2"
   },
@@ -469,7 +470,8 @@ function redzlib:MakeWindow(Configs)
   local MainFrame = InsertTheme(Create("ImageButton", ScreenGui, {
     Size = UDim2.fromOffset(UISizeX, UISizeY),
     Position = UDim2.new(0.5, -UISizeX/2, 0.5, -UISizeY/2),
-    BackgroundTransparency = 0.03,
+    BackgroundTransparency = 0.04,
+    Image = "http://www.roblox.com/asset/?id=18363452575"
     Name = "Hub"
   }), "Main")Make("Gradient", MainFrame, {
     Rotation = 45
