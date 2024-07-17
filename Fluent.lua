c8de0ced7496833c02e918aae1e2f6369e24f807
Wuynnz <158767658+Wuynnz@users.noreply.github.com>
2024-07-18 04:37:44 +0700

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -3349,22 +3349,30 @@ local aa = {
         return c
     end,
 [25] = function()
-        local aa, ab, ac, ad, ae = b(25)
-        local af = ab.Parent.Parent
-        local ag, ah, ai, aj = af.Components, ac(af.Packages.Flipper), ac(af.Creator), {}
-        aj.__index = aj
-        aj.__type = "Paragraph"
-        function aj.New(c, d)
-            assert(d.Title, "Paragraph - Missing Title")
-            d.Content = d.Content or ""
-            local e = ac(ag.Element)(d.Title, d.Content, aj.Container, false)
-            e.Frame.BackgroundTransparency = 1
-            e.Border.Transparency = 0.6
-            return e
+    local aa, ab, ac, ad, ae = b(25)
+    local af = ab.Parent.Parent
+    local ag, ah, ai, aj = af.Components, ac(af.Packages.Flipper), ac(af.Creator), {}
+    aj.__index = aj
+    aj.__type = "Paragraph"
+    
+    function aj.New(c, d)
+        assert(d.Title, "Paragraph - Missing Title")
+        d.Content = d.Content or ""
+        local e = ac(ag.Element)(d.Title, d.Content, aj.Container, false)
+        e.Frame.BackgroundTransparency = 1
+        e.Border.Transparency = 0.6
+        return e
+    end
+    
+    function aj:SetValue(title, content)
+        if self.Frame and self.Content then
+            self.Frame.Title.Text = title
+            self.Content.Text = content
         end
-        return aj
-    end,
+    end
 
+    return aj
+end,
     [26] = function()
         local aa, ab, ac, ad, ae = b(26)
         local af, ag = game:GetService "UserInputService", ab.Parent.Parent
