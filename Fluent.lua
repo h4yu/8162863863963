0a8bfecd5d0038bb15ef8a2ed9888ae8faaf84f8
Wuynnz <158767658+Wuynnz@users.noreply.github.com>
2024-07-17 20:56:54 +0700

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -3348,22 +3348,36 @@ local aa = {
         end
         return c
     end,
-    [25] = function()
-        local aa, ab, ac, ad, ae = b(25)
-        local af = ab.Parent.Parent
-        local ag, ah, ai, aj = af.Components, ac(af.Packages.Flipper), ac(af.Creator), {}
-        aj.__index = aj
-        aj.__type = "Paragraph"
-        function aj.New(c, d)
-            assert(d.Title, "Paragraph - Missing Title")
-            d.Content = d.Content or ""
-            local e = ac(ag.Element)(d.Title, d.Content, aj.Container, false)
-            e.Frame.BackgroundTransparency = 1
-            e.Border.Transparency = 0.6
-            return e
-        end
-        return aj
-    end,
+[25] = function()
+    local aa, ab, ac, ad, ae = b(25)
+    local af = ab.Parent.Parent
+    local ag, ah, ai, aj = af.Components, ac(af.Packages.Flipper), ac(af.Creator), {}
+    aj.__index = aj
+    aj.__type = "Paragraph"
+
+    function aj.New(c, d)
+        assert(d.Title, "Paragraph - Missing Title")
+        d.Content = d.Content or ""
+        local e = ac(ag.Element)(d.Title, d.Content, aj.Container, false)
+        e.Frame.BackgroundTransparency = 1
+        e.Border.Transparency = 0.6
+
+        local self = setmetatable({}, aj)
+        self.Element = e
+        self.Title = d.Title
+        self.Content = d.Content
+        return self
+    end
+
+    function aj:SetValue(title, content)
+        self.Title = title
+        self.Content = content or ""
+        self.Element.Title.Text = self.Title
+        self.Element.Content.Text = self.Content
+    end
+
+    return aj
+end,
     [26] = function()
         local aa, ab, ac, ad, ae = b(26)
         local af, ag = game:GetService "UserInputService", ab.Parent.Parent
