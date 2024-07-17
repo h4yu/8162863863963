81ec7381ccccf60b0062ecd2dbf36bba2722df5e
Wuynnz <158767658+Wuynnz@users.noreply.github.com>
2024-07-18 04:45:08 +0700

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -3361,9 +3361,9 @@ local aa = {
         local e = ac(ag.Element)(d.Title, d.Content, aj.Container, false)
         e.Frame.BackgroundTransparency = 1
         e.Border.Transparency = 0.6
-        return e
+        return setmetatable({Frame = e.Frame, Content = e.Content}, aj)
     end
-    
+
     function aj:SetValue(title, content)
         if self.Frame and self.Content then
             self.Frame.Title.Text = title
