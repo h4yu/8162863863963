edf2feb98bf9140ad3e90e75914edd73f447e9c3
Wuynnz <158767658+Wuynnz@users.noreply.github.com>
2024-07-18 12:15:13 +0700

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -462,13 +462,13 @@ local aa = {
                     j(
                         "ImageLabel",
                         {
-                            Image = "rbxassetid://18481502275",
+                            Image = "rbxassetid://1",
                             ImageTransparency = 0.9,
                             ScaleType = Enum.ScaleType.Stretch,
                             TileSize = UDim2.new(0, 128, 0, 128),
                             Size = UDim2.fromScale(1, 1),
                             BackgroundTransparency = 1,
-                            ThemeTag = {ImageTransparency = "AcrylicNoise"}
+                            ThemeTag = {ImageTransparency = "AcrylicNoise", Image = "ImageHub"}
                         },
                         {j("UICorner", {CornerRadius = UDim.new(0, 8)})}
                     ),
@@ -1592,7 +1592,7 @@ local aa = {
                     l(
                         "Frame",
                         {
-                            BackgroundTransparency = 0.5,
+                            BackgroundTransparency = 1,
                             Size = UDim2.new(1, 0, 0, 1),
                             Position = UDim2.new(0, 0, 1, 0),
                             ThemeTag = {BackgroundColor3 = "TitleBarLine"}
@@ -5184,6 +5184,7 @@ local aa = {
             AcrylicBorder = Color3.fromRGB(90, 90, 90),
             AcrylicGradient = ColorSequence.new(Color3.fromRGB(40, 40, 40), Color3.fromRGB(40, 40, 40)),
             AcrylicNoise = 0,
+	    ImageHub = "rbxassetid://18481502275",
             TitleBarLine = Color3.fromRGB(75, 75, 75),
             Tab = Color3.fromRGB(237, 169, 255),
             Element = Color3.fromRGB(120, 120, 120),
